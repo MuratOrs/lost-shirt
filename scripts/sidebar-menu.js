b6f0cd67dd8c863fb967e2d8e03622dddb2695dc
Murat Orsthoyev <muratorsthoyev@Murat.local>
2020-05-15 01:27:00 +0200

    initial commit

---
+++ scripts/sidebar-menu.js
@@ -0,0 +1,15 @@
+(function(){
+    const openingBtn = document.querySelector('.sidebar__hamburger');
+    const closeBtn = document.querySelector('.sidebar__close');
+    const sidebar = document.querySelector('.sidebar');
+    
+    openingBtn.addEventListener("click", function(){
+        sidebar.classList.add('sidebar--opened');
+    }); 
+    
+    
+    closeBtn.addEventListener("click", function(){
+        sidebar.classList.remove('sidebar--opened');
+    });     
+    
+}());
